fe97cb0f9bffa4ee3700f92bfeca69300495e37b
Sunny996 <satlaakash@gmail.com>
2022-04-08 21:11:02 +0530

    ADDED: A method to append nodes at the end of linked list

--- src/main/MyLinkedListTest.java
+++ src/main/MyLinkedListTest.java
@@ -5,6 +5,22 @@ import org.junit.Test;
 
 public class MyLinkedListTest {
     @Test
+    public void given3NumbersWhenAddedToLinkedListShouldBeAddedToTop() {
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.addNode(myFirstNode);
+        myLinkedList.addNode(mySecondNode);
+        myLinkedList.addNode(myThirdNode);
+
+        boolean result = myLinkedList.getHead().equals(myThirdNode) &&
+                myLinkedList.getHead().getNext().equals(mySecondNode) &&
+                myLinkedList.getTail().equals(myFirstNode);
+        Assert.assertTrue(result);
+    }
+    @Test
     public void given3NumbersWhenAddedToLinkedListShouldBeAddedToLast() {
         MyNode<Integer> myFirstNode = new MyNode<>(56);
         MyNode<Integer> mySecondNode = new MyNode<>(30);
